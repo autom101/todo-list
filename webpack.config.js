0d36525dae813df990e5660dd2e40cca8c8d689b
sojha2 <122489814+sojha2@users.noreply.github.com>
2023-03-27 19:33:36 -0600

    Set up initial files

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    index: "./src/js/index.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "To-do List",
+    }),
+  ],
+  output: {
+    filename: "[name].main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
