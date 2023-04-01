361c6ba413bce4062a7414d99b53b5630b106e76
sojha2 <122489814+sojha2@users.noreply.github.com>
2023-03-31 20:10:36 -0600

    Display project names on web page

--- src/js/dom.js
+++ src/js/dom.js
@@ -17,7 +17,25 @@ const createSidebarHeader = () => {
   return sidebarHeader;
 };
 
-const createSidebarMain = () => {
+const displayProjects = (projectsContainer) => {
+  const projects = document.createElement("main");
+
+  const projectObjects = Object.keys(projectsContainer);
+
+  let index = 0;
+  projectObjects.forEach((project) => {
+    projectObjects[index] = document.createElement("h3");
+    projectObjects[index].textContent = projectsContainer[project].name;
+
+    projects.appendChild(projectObjects[index]);
+
+    index++;
+  });
+
+  return projects;
+};
+
+const createSidebarMain = (projectsContainer) => {
   const sidebarMain = document.createElement("main");
   sidebarMain.classList.add("sidebar-main");
 
@@ -34,33 +52,34 @@ const createSidebarMain = () => {
   sidebarMainHeading.appendChild(sidebarPlusSign);
 
   sidebarMain.appendChild(sidebarMainHeading);
+  sidebarMain.appendChild(displayProjects(projectsContainer));
 
   return sidebarMain;
 };
 
-const createSidebar = () => {
+const createSidebar = (projectsContainer) => {
   const sidebar = document.createElement("section");
   sidebar.classList.add("sidebar");
 
   sidebar.appendChild(createSidebarHeader());
-  sidebar.appendChild(createSidebarMain());
+  sidebar.appendChild(createSidebarMain(projectsContainer));
 
   return sidebar;
 };
 
-const createTodoMain = () => {
+const createTodoMain = (projectsContainer) => {
   const todoMain = document.createElement("section");
   todoMain.classList.add("todo-main");
   return todoMain;
 };
 
-const createTodoUI = () => {
+const createTodoUI = (projectsContainer) => {
   //
   const todoContainer = document.createElement("main");
   todoContainer.classList.add("todo-container");
 
-  todoContainer.appendChild(createSidebar());
-  todoContainer.appendChild(createTodoMain());
+  todoContainer.appendChild(createSidebar(projectsContainer));
+  todoContainer.appendChild(createTodoMain(projectsContainer));
   return todoContainer;
 };
 
--- src/js/index.js
+++ src/js/index.js
@@ -3,18 +3,19 @@ import { createProject } from "./projects.js";
 import { createToDo } from "./to-dos.js";
 import { createTodoUI } from "./dom.js";
 
-//import css
+//import css files
 import "../css/reset.css";
 import "../css/styles.css";
 
-//import images
+//import img files
 import "../img/side-bar-images/pencil-outline.svg";
 
 const body = document.querySelector("body");
-body.appendChild(createTodoUI());
-
 const My_Projects = {};
 
-My_Projects[`Test`] = createProject(`Test`);
-My_Projects[`Test`][`TestToDo`] = createToDo(`TestToDo`);
+My_Projects["0"] = createProject("Test");
+My_Projects["1"] = createProject("Test2");
+My_Projects["2"] = createProject("Slkjasdf");
 console.log(My_Projects);
+
+body.appendChild(createTodoUI(My_Projects));
