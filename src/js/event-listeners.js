9251abdfa28da0d2d190a7d10e8585854ccdd7b7
sojha2 <122489814+sojha2@users.noreply.github.com>
2023-04-01 20:29:22 -0600

    Add strikethrough functionality to todos

--- src/js/event-listeners.js
+++ src/js/event-listeners.js
@@ -17,11 +17,28 @@ const createProjectTitleListener = (projectsObject, createProject) => {
   });
 };
 
+const handleTodoStrikeThrough = (todoTitle, newTodo) => {
+  const strikethroughDiv = document.createElement("div");
+  strikethroughDiv.classList.add("strikethrough-todo");
+
+  todoTitle.addEventListener("click", (e) => {
+    if (todoTitle.contains(strikethroughDiv)) {
+      todoTitle.removeChild(strikethroughDiv);
+    } else {
+      todoTitle.appendChild(strikethroughDiv);
+    }
+    todoTitle.classList.toggle("crossed");
+  });
+};
+
 const handleFormSubmit = (todoForm, createToDo) => {
   const formData = new FormData(todoForm);
   const todoTitle = document.createElement("li");
+  todoTitle.classList.add("to-do");
   todoTitle.textContent = formData.get("todo-title");
   const newTodo = createToDo(todoTitle.textContent);
+
+  handleTodoStrikeThrough(todoTitle, newTodo);
 
   const todoList = document.querySelector(".todo-list");
   todoList.insertAdjacentElement("beforeend", todoTitle);
