eee93e9fce9c90abc1efc23e852378c23ee9607a
Sameer Ojha <122489814+autom101@users.noreply.github.com>
2023-12-02 19:10:09 -0700

    Add basic bootstrap formatting in the app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,24 @@
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Sidebar } from "./components";
 
 function App() {
   return (
     <Router>
-      <Sidebar></Sidebar>
-      <Routes>
-        <Route path="/" />
-      </Routes>
+      <Container className="vh-100 vw-100 m-0">
+        <Row className="h-100">
+          <Col className="col-md-3 border h-100">
+            <Sidebar></Sidebar>
+          </Col>
+          <Col>
+            <Routes>
+              <Route path="/" />
+            </Routes>
+          </Col>
+        </Row>
+      </Container>
     </Router>
   );
 }
--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,8 +1,24 @@
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import pic from "../assets/x-symbol.svg";
+
 const Sidebar = () => {
   return (
-    <section>
-      <h1>Todo List</h1>
-    </section>
+    <Container className="pt-3">
+      <Row className="mb-5">
+        <Col>
+          <img
+            className="img-fluid border rounded-circle w-50"
+            src={pic}
+            alt="A placeholder for now"
+          />
+        </Col>
+        <Col>Person</Col>
+      </Row>
+      <Row>Today</Row>
+      <Row>Projects</Row>
+    </Container>
   );
 };
 
