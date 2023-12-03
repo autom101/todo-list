236c956ddb0df8a3eaa08b23707c99abed533bad
Sameer Ojha <122489814+autom101@users.noreply.github.com>
2023-12-02 18:30:55 -0700

    Add bootstrap to the project

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/* eslint-disable no-undef */
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default {
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "~bootstrap": path.resolve(__dirname, "node_modules/bootstrap"),
+    },
+  },
+  server: {
+    port: 8080,
+    hot: true,
+  },
+};
