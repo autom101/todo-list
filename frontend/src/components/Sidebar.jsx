4f3d76a5bdecad9f645349b5c75394f37e6304c8
Sameer Ojha <122489814+autom101@users.noreply.github.com>
2023-12-24 12:23:26 -0700

    Create basic structure for the sidebar content

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,10 +1,25 @@
+import { Link } from "react-router-dom";
+
 const Sidebar = () => {
   return (
-    <div className="flex flex-col w-56 h-screen px-4 pt-4 overflow-y-scroll border-2">
-      <div className="flex justify-between w-full">
+    <div className="flex flex-col w-56 h-screen px-4 pt-4 overflow-y-scroll gap-y-6 border-x-2 border-amber-800">
+      <section className="flex justify-between w-full">
         <div>Pic</div>
-        <div>Name</div>
-      </div>
+        <h2>Name</h2>
+      </section>
+      <section className="ms-6">
+        <h2 className="text-2xl">
+          <Link to="/today">Today</Link>
+        </h2>
+      </section>
+      <section className="ms-6">
+        <h2 className="text-2xl">Projects</h2>
+        <ul className="pl-6 list-circle">
+          <li>
+            <Link>Sample Project</Link>
+          </li>
+        </ul>
+      </section>
     </div>
   );
 };
